c4c6642e6e6d2c2f799406077dbd346616e69e33
chanelnalani <chanelnalani@gmail.com>
2018-04-03 12:46:04 -0700

    Minor Changes and Cleanup

--- src/test/java/com/build/qa/build/selenium/tests/BuildTest.java
+++ src/test/java/com/build/qa/build/selenium/tests/BuildTest.java
@@ -4,11 +4,11 @@ package com.build.qa.build.selenium.tests;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import com.build.qa.build.selenium.framework.BaseFramework;
+import com.build.qa.build.selenium.pageobjects.homepage.AddToCartPage;
 import com.build.qa.build.selenium.pageobjects.homepage.HomePage;
 import com.build.qa.build.selenium.pageobjects.elements.*;
 
@@ -48,18 +48,18 @@ public class BuildTest extends BaseFramework {
 	/** 
 	 * Search for the Quoizel MY1613 from the search bar
 	 * @assert: That the product page we land on is what is expected by checking the product title
+	 * @difficulty Easy
 	 */
 	@Test
 	public void searchForProductLandsOnCorrectProduct() { 
 		// TODO: Implement this test
-	driver.get(getConfiguration("HOMEPAGE"));
-
-	HomePage homePage = new HomePage(driver, wait);
-	homePage.closeWelcomeDialog();
-	homePage.search("Quoizel MY1613");
-
-	Assert.assertEquals("Quoizel MY1613ML", homePage.getProductTitle());
-
+		
+		driver.get(getConfiguration("HOMEPAGE"));
+		HomePage homePage = new HomePage(driver, wait);
+		
+		softly.assertThat(homePage.onBuildTheme())
+			.as("The website should load up with the Build.com desktop theme.")
+			.isTrue();
     }
 	
 //	/** 
@@ -71,71 +71,36 @@ public class BuildTest extends BaseFramework {
 	public void addProductToCartFromCategoryDrop() { 
 		// TODO: Implement this test
 		driver.get(getConfiguration("HOMEPAGE") + "/bathroom-sinks/c108504");
-
-		HomePage homePage = new HomePage(driver, wait);
-		homePage.closeWelcomeDialog();
-	  
-	  Cart cart = new Cart(driver, wait);
-	  Assert.assertEquals(cart.addProductToCartByIndex(2), cart.getProductTitleInCart());
-	  
+		BathroomSinksPage sinksPage= new BathroomSinksPage(driver, wait);
+		AddToCartPage cartPage=sinksPage.addSecondProductToCart();
+		softly.assertThat(cartPage.getProductText())
+			.as("The search text result should be have text containing Kohler K-2214")
+			.contains("Kohler K-2214");
 	  
 	}
 	
-//	/** 
-//	 * Add a product to the cart and email the cart to yourself, also to my email address: chanelnalani@gmail.com
-//	 * Include this message in the "message field" of the email form: "This is {yourName}, sending you a cart from my automation!"
-//	 * @assert that the "Cart Sent" success message is displayed after emailing the cart
-//	 */
+	/** 
+	 * Add a product to the cart and email the cart to yourself, also to my email address: jgilmore+SeleniumTest@build.com
+	 * Include this message in the "message field" of the email form: "This is {yourName}, sending you a cart from my automation!"
+	 * @assert that the "Cart Sent" success message is displayed after emailing the cart
+	 * @difficulty Medium-Hard
+	 */
 	@Test
 	public void addProductToCartAndEmailIt() { 
 		// TODO: Implement this test
-		driver.get(getConfiguration("HOMEPAGE") + "/bathroom-sinks/c108504");
 
-		HomePage homePage = new HomePage(driver, wait);
-		homePage.closeWelcomeDialog();
-
-		Cart cartProduct = new Cart(driver, wait);
-		cartProduct.addProductToCartByIndex(2);
-		cartProduct.openCart();
-
-		Email email = new Email();
-		email.setYourName(getConfiguration("yourName"));
-		email.setYourEmail(getConfiguration("yourEmail"));
-		email.setRecipientName(getConfiguration("recipientName"));
-		email.setRecipientEmail(getConfiguration("recipientEmail"));
-		email.setMessage(getConfiguration("message"));
-
-	  
-	  Cart.email(email);
-	  
 	}
 	
-//	/** 
-//	 * Go to a category drop page (such as Bathroom Faucets) and narrow by
-//	 * at least two filters (facets), e.g: Finish=Chromes and Theme=Modern
-//	 * @assert that the correct filters are being narrowed, and the result count
-//	 * is correct, such that each facet selection is narrowing the product count.
-//	 */
+	/** 
+	 * Go to a category drop page (such as Bathroom Faucets) and narrow by
+	 * at least two filters (facets), e.g: Finish=Chromes and Theme=Modern
+	 * @assert that the correct filters are being narrowed, and the result count
+	 * is correct, such that each facet selection is narrowing the product count.
+	 * @difficulty Hard
+	 */
 	@Test
 	public void facetNarrowBysResultInCorrectProductCounts() { 
 		// TODO: Implement this test		
-	driver.get(getConfiguration("HOMEPAGE"));
-
-	HomePage homePage = new HomePage(driver, wait);
-	homePage.closeWelcomeDialog();
-	homePage.clickBathroomTab();
-	homePage.clickByName(bath.BathroomFaucets);
-
-	Cart cartFilter = new Cart(driver, wait);
-	String count = cartFilter.filterProductByColor(Color.Chromes);
-	String totalCount = cartFilter.getProductResultCount();
-
-	Assert.assertEquals(count, totalCount);
-
-	count = cartFilter.filterProductByTheme(Filter.Modern);
-	totalCount = cartFilter.getProductResultCount();
-
-	Assert.assertEquals(count, totalCount);
 
 	}
 	
